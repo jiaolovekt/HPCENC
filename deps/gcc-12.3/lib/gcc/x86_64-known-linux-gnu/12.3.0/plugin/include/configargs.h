8feea6bb0d1c83f2ccba89d6490ea30bbacf2c8f
Jia <jiao.ktxp@hotmail.com>
2023-06-05 18:07:13 +0800

    initial bare environment commit, x264/265 with avs libass working. ffmpeg encode aac and mux working.

---
+++ deps/gcc-12.3/lib/gcc/x86_64-known-linux-gnu/12.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-12.3.0/configure --build=x86_64-known-linux-gnu --sysconfdir=/etc --localstatedir=/var --prefix=/home/software/gcc/12.3 --libexecdir='/home/software/gcc/12.3/lib' --enable-version-specific-runtime-libs --disable-rpath --with-system-zlib --enable-languages=c,c++,lto,fortran --enable-initfini-array --enable-gnu-indirect-function --without-included-gettext --enable-threads=posix --libdir='/home/software/gcc/12.3/lib' --enable-nls --enable-bootstrap --enable-clocale=gnu --enable-libstdcxx-debug --enable-libstdcxx-time=yes --with-default-libstdcxx-abi=new --enable-gnu-unique-object --disable-vtable-verify --enable-libmpx --enable-plugin --enable-default-pie --with-system-zlib --with-target-system-zlib --enable-objc-gc=auto --enable-multiarch --disable-werror --with-abi=m64 --with-multilib-list=m64 --enable-multilib";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "m64" }, { "cpu", "generic" }, { "arch", "x86-64" } };
