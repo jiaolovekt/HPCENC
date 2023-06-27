852f7b55b6cf8e301704aebdd5e3e1b601d346f2
WHR <msl0000023508@gmail.com>
2023-06-27 19:15:20 +0800

    fix relapath;

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-export ENCROOT="$PWD" && module use "$ENCROOT/deps/modfiles"
+if [ -z "$ENCROOT" ] ; then
+	export ENCROOT="$PWD" 
+fi
+module use "$ENCROOT/deps/modfiles"
 
-module load avsp3.7.2  cython  DevIL  ffmpeg-5-pre  ffms2  fribidi  gcc-12.3  harfbuzz-7.3.0  hpcenc  libass-0.17.1  libfdk_aac  lsmash-2.14.5  opencl  python-3.11.3  vs-r62  x264-b-broadwell  x265-p-broadwell  zimg-3.0.4
+module load avsp3.7.2  cython  DevIL  ffmpeg5-gcc12  ffms2  fribidi  gcc-12.3  harfbuzz-7.3.0  hpcenc  libass-0.17.1  libfdk_aac  lsmash-2.14.5  opencl  python-3.11.3 x264-gcc12-broadwell x265-gcc12-broadwell vs-r62    zimg-3.0.4
