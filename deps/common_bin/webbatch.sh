0db39fc5339fd68dbe7640183cabeda515445e66
jiaolovekt <getr18@163.com>
2023-07-07 03:56:01 +0800

    experimentail conditional x264 threads limit and socket pin;

--- deps/common_bin/webbatch.sh
+++ deps/common_bin/webbatch.sh
@@ -13,10 +13,13 @@ fi
 SLURM_TEMPLATE=
 
 #getopt
-while getopts P:p:n:i:r:l:m:c:t:h OP ; do
+while getopts P:N:p:n:i:r:l:m:c:t:h OP ; do
         case "$OP" in
 	P)
 		PART="$OPTARG"
+	;;
+	N)
+		NTHREADS="$OPTARG"
 	;;
         p)
                 PROFILE="$OPTARG"
@@ -49,9 +52,40 @@ while getopts P:p:n:i:r:l:m:c:t:h OP ; do
         ;;
         esac
 done
+#limit 264 sub within 1 socket
+#State=UP TotalCPUs=16000 TotalNodes=400 SelectTypeParameters=NONE
+if [ -z "$NTHREADS" ] && [ "$CODEC" = 264 ]  ; then
+	NTHREADS=0
+	local State=
+	local TotalCPUs=
+	local TotalNodes=
+	local SelectTypeParameters=
+	if [ -n "$PART" ] && ! echo "$PART" | grep -Fq ',' ; then
+	#only process single partition passwd
+		eval $(scontrol show partition "$PART" | grep TotalCPUS)
+	else
+	# process default partition
+		eval $(scontrol show partition | grep -F 'Default=YES' -A6 | grep TotalCPUs)
+	fi
+	if [ -n "$TotalCPUs" ] && [ -n "$TotalNodes" ] ; then
+	#calc single socket, assume DP not MP
+		NTHREADS=$((TotalCPUS/TotalNodes/2))
+	fi
+	# apply only when 16 <= threads <=32
+	if [ "$NTHREADS" -ge 16 ] && [ "$NTHREADS" -le 32 ] ; then
+		echo "use $NTHREADS threads for $CODEC job"
+		THREADS="-n $NTHREADS"
+	fi
+elif [ -n "$NTHREADS" ] ; then
+	echo "use $NTHREADS threads for $CODEC job"
+	THREADS="-n $NTHREADS"
+fi
+				
+		
 [ -n "$PART" ] && PART="-p $PART"
 [ -z "$SLURM_TEMPLATE" ] && SLURM_TEMPLATE="${ENCROOT}/config/sub_web"
 [ -n "$MODE" ] && MODE=".${MODE}"
 JNAME="${PROJECT}_${INDEX}_${LANGG}_${RESO}_${CODEC}${MODE}"
 
-sbatch $PART -o "$JNAME".log -e "$JNAME".log -J "$JNAME" --export=ALL,PROFILE="$PROFILE",PROJECT="$PROJECT",INDEX="$INDEX",RESO="$RESO",LANGG="$LANGG",CODEC="$CODEC",MODE="$MODE" "$SLURM_TEMPLATE"
+
+sbatch $PART $THREADS -o "$JNAME".log -e "$JNAME".log -J "$JNAME" --export=ALL,PROFILE="$PROFILE",PROJECT="$PROJECT",INDEX="$INDEX",RESO="$RESO",LANGG="$LANGG",CODEC="$CODEC",MODE="$MODE" "$SLURM_TEMPLATE"
