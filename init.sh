7b6b3055a4df067f3d6c7730dea865615cca3516
jiaolovekt <getr18@163.com>
2023-07-07 20:49:01 +0800

    add module check;

--- init.sh
+++ init.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+if ! module > /dev/null ; then
+	echo "environment modules not found or not configured on this cluster at the current shell."
+	echo "for details, check https://github.com/cea-hpc/modules"
+	return 1
+fi
+
 if [ -z "$ENCROOT" ] ; then
 	export ENCROOT="$PWD" 
 fi
 module use "$ENCROOT/deps/modfiles"
 
 module load avsp3.7.2  cython  DevIL  ffmpeg-5.1.3  ffms2  gcc-12.3  hpcenc  lsmash-2.14.5  opencl  python-3.11.3 x264-gcc12-broadwell x265-gcc12-broadwell vs-r62    zimg-3.0.4 mkvtoolnix-78
+. config/func
