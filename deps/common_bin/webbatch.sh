388308849dcfcc3d1bcced51a50e13d345aa4d81
jiaolovekt <getr18@163.com>
2023-07-07 17:35:05 +0800

    x264 socket binding;

--- deps/common_bin/webbatch.sh
+++ deps/common_bin/webbatch.sh
@@ -2,7 +2,7 @@
 
 usage()
 {
-	echo "Usage: webbatch [ -P partition -p profile ] -n projectname -i index -r [1080/720] -l [GB/B5] -c [264/265] [ -t job_template ] [ -m avs/vs ]" 1>&2
+	echo "Usage: webbatch [ -P partition -N nthreads -p profile ] -n projectname -i index -r [1080/720] -l [GB/B5] -c [264/265] [ -t job_template ] [ -m avs/vs ]" 1>&2
 	exit 0
 }
 #checkopt
@@ -52,40 +52,66 @@ while getopts P:N:p:n:i:r:l:m:c:t:h OP ; do
         ;;
         esac
 done
-#limit 264 sub within 1 socket
 #State=UP TotalCPUs=16000 TotalNodes=400 SelectTypeParameters=NONE
-if [ -z "$NTHREADS" ] && [ "$CODEC" = 264 ]  ; then
+#Adaptive thread control
+if [ -z "$NTHREADS" ] ; then
+	if ! [ -x $(which scontrol) ] ; then
+		echo "scontrol not found, need specify -N manually"
+	fi
 	NTHREADS=0
-	local State=
-	local TotalCPUs=
-	local TotalNodes=
-	local SelectTypeParameters=
+	State=
+	TotalCPUs=
+	TotalNodes=
+	SelectTypeParameters=
 	if [ -n "$PART" ] && ! echo "$PART" | grep -Fq ',' ; then
 	#only process single partition passwd
-		eval $(scontrol show partition "$PART" | grep TotalCPUS)
-	else
+		eval $(scontrol show partition "$PART" | grep -F 'TotalCPUs')
+		if [ -n "$TotalCPUs" ] && [ -n "$TotalNodes" ] ; then
+		#calc single socket, assume DP not MP
+			NTHREADS=$((TotalCPUs/TotalNodes/2))
+		fi
+	elif [ -z "$PART" ] ; then
 	# process default partition
-		eval $(scontrol show partition | grep -F 'Default=YES' -A6 | grep TotalCPUs)
+		eval $(scontrol show partition | grep -F 'Default=YES' -A6 | grep -F 'TotalCPUs')
+		if [ -n "$TotalCPUs" ] && [ -n "$TotalNodes" ] ; then
+			NTHREADS=$((TotalCPUs/TotalNodes/2))
+		fi
+	else
+	# process all partition, use minimal core per socket
+	eval $(scontrol show partition | grep -F 'TotalCPUs' | ( while read ln ; do
+		eval "$ln"
+		if [ -n "$TotalCPUs" ] && [ -n "$TotalNodes" ] ; then
+			CUTHREADS=$((TotalCPUs/TotalNodes/2))
+			[ "$NTHREADS" = 0 ] && NTHREADS="$CUTHREADS"
+			[ "$NTHREADS" -gt "$CUTHREADS" ] && NTHREADS="$CUTHREADS" 
+		fi
+		done
+		echo "NTHREADS=$NTHREADS" ))
 	fi
-	if [ -n "$TotalCPUs" ] && [ -n "$TotalNodes" ] ; then
-	#calc single socket, assume DP not MP
-		NTHREADS=$((TotalCPUS/TotalNodes/2))
+	# assume 2 x 12 core if cannot get info
+	[ "$NTHREADS" = 0 ] && NTHREADS=12 && echo "no partition info, use $NTHREADS threads per socket"
+	#limit 264 sub within 1 socket if appropriate cores
+	if [ "$CODEC" = 264 ]  ; then
+		# apply only when 16 <= threads <=32
+		if [ "$NTHREADS" -ge 16 ] && [ "$NTHREADS" -le 32 ] ; then
+			#THREADS="-n $NTHREADS --sockets-per-node 1"
+			THREADS="-n $NTHREADS"
+		elif [ "$NTHREADS" -lt 16 ] ; then
+			THREADS="-n 16"
+		else
+			THREADS="-n 32"
+		fi
+	else
+		#full node batch
+		THREADS="-n $((NTHREADS*2))"
 	fi
-	# apply only when 16 <= threads <=32
-	if [ "$NTHREADS" -ge 16 ] && [ "$NTHREADS" -le 32 ] ; then
-		echo "use $NTHREADS threads for $CODEC job"
-		THREADS="-n $NTHREADS"
-	fi
-elif [ -n "$NTHREADS" ] ; then
-	echo "use $NTHREADS threads for $CODEC job"
-	THREADS="-n $NTHREADS"
 fi
-				
 		
+echo "use $NTHREADS threads for $CODEC job"
 [ -n "$PART" ] && PART="-p $PART"
 [ -z "$SLURM_TEMPLATE" ] && SLURM_TEMPLATE="${ENCROOT}/config/sub_web"
-[ -n "$MODE" ] && MODE=".${MODE}"
-JNAME="${PROJECT}_${INDEX}_${LANGG}_${RESO}_${CODEC}${MODE}"
+[ -n "$MODE" ] && SMODE=".${MODE}"
+JNAME="${PROJECT}_${INDEX}_${LANGG}_${RESO}_${CODEC}${SMODE}"
 
 
-sbatch $PART $THREADS -o "$JNAME".log -e "$JNAME".log -J "$JNAME" --export=ALL,PROFILE="$PROFILE",PROJECT="$PROJECT",INDEX="$INDEX",RESO="$RESO",LANGG="$LANGG",CODEC="$CODEC",MODE="$MODE" "$SLURM_TEMPLATE"
+sbatch $PART $THREADS --distribution=block:block -N 1 -o "$JNAME".log -e "$JNAME".log -J "$JNAME" --export=ALL,PROFILE="$PROFILE",PROJECT="$PROJECT",INDEX="$INDEX",RESO="$RESO",LANGG="$LANGG",CODEC="$CODEC",MODE="$MODE" "$SLURM_TEMPLATE"
